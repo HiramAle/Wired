8373427257638f2bda1c4255467032e02f8c3b60
Hiram Alejandro Gomez Garibay <magicalex@outlook.es>
2023-06-03 08:57:12 -0600

    👷Added multiple pages for task page

--- src/scenes/pause_menu/jobs.py
+++ src/scenes/pause_menu/jobs.py
@@ -8,6 +8,7 @@ from engine.ui.text import Text
 from src.scenes.pause_menu.pause_objects import PlayerAvatar, ItemSlot
 from engine.task_manager import TaskManager
 from engine.playerdata import PlayerData
+from engine.ui.button import Button
 
 
 class TaskLabel(Sprite):
@@ -27,25 +28,60 @@ class Jobs(Scene):
         super().__init__("jobs")
         self.jobs = SpriteGroup()
         self.ui = SpriteGroup()
-
+        self.page_index = 0
         Text((143 + 8, 42), "Trabajos", 32, Colors.SPRITE, self.ui, centered=False)
-        
+        self.pages = []
+        self.tasks = []
         for index, task in enumerate(PlayerData.tasks.current_tasks):
-            TaskLabel((85 + 8, 90 + (index * 52)), task.id, self.jobs)
+            if index % 5 == 0 and index != 0:
+                self.pages.append(self.tasks)
+                self.tasks = []
+            self.tasks.append(task)
+
+        self.render_tasks()
         self.task_title_frame = Sprite((346 + 8, 46), Assets.images_book["task_name"], self.ui, centered=False)
         self.task_title = Text(self.task_title_frame.center, "", 16, Colors.SPRITE, self.ui)
         self.task_description_frame = Sprite((351 + 8, 105), Assets.images_book["task_description"], self.ui,
                                              centered=False)
         self.task_description = Text(self.task_description_frame.center, "", 16, Colors.SPRITE, self.ui, max_width=160)
+        self.left_button = Button((152, 297), Assets.images_book["button_left_normal"],
+                                  Assets.images_book["button_left_pressed"], centered=False)
+        self.right_button = Button((219, 297), Assets.images_book["button_right_normal"],
+                                   Assets.images_book["button_right_pressed"], centered=False)
+
+    @property
+    def current_page(self) -> list:
+        return self.pages[self.page_index]
 
     def update(self) -> None:
+        self.left_button.update()
+        self.right_button.update()
         for job in self.jobs.sprites():
             job: TaskLabel
             if not job.hovered:
                 continue
             self.task_title.text = job.task.title
             self.task_description.text = job.task.description
+        if self.left_button.clicked:
+            self.page_index -= 1
+            if self.page_index < 0:
+                self.page_index = 0
+            else:
+                self.render_tasks()
+        if self.right_button.clicked:
+            self.page_index += 1
+            if self.page_index >= len(self.pages):
+                self.page_index = len(self.pages) - 1
+            else:
+                self.render_tasks()
+
+    def render_tasks(self):
+        self.jobs = SpriteGroup()
+        for index, task in enumerate(self.current_page):
+            TaskLabel((85 + 8, 80 + (index * 42)), task.id, self.jobs)
 
     def render(self) -> None:
         self.jobs.render(self.display)
         self.ui.render(self.display)
+        self.left_button.render(self.display)
+        self.right_button.render(self.display)
